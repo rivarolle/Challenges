e1ad7524ea81e118cf54bceb896cb7afc471fbae
Hussein Kaddoura <hussein.kaddoura@gettyimages.com>
2020-03-11 15:11:46 -0600

    KSum

--- Challenges/Algorithms/Easy/MinMaxSum.cs
+++ Challenges/Algorithms/Easy/MinMaxSum.cs
@@ -8,37 +8,12 @@ namespace Challenges.Algorithms.Easy
         static void miniMaxSum(long[] arr)
         {
             //sort the array
-            QuickSort(arr);
+            Utils.Quicksort(arr, arr.Length - 1);
 
             var min = arr[0] + arr[1] + arr[2] + arr[3];
             var max = arr[4] + arr[1] + arr[2] + arr[3];
 
             Console.WriteLine($"{min} {max}");
-        }
-
-        static void QuickSort(long[] arr)
-        {
-            pivotArray(arr, 0, arr.Length - 1);
-        }
-
-        static void pivotArray(long[] arr, int startIndex, int pivotIndex)
-        {
-            var lowIndex = startIndex;
-            for(int j=startIndex; j<= pivotIndex; j++)
-            {
-                if (arr[j] <= arr[pivotIndex])
-                {
-                    var currentValue = arr[lowIndex];
-                    arr[lowIndex] = arr[j];
-                    arr[j] = currentValue;
-                    lowIndex++;
-                }
-            }
-            
-            if(lowIndex > 2)
-                pivotArray(arr, 0, lowIndex-2);
-            if(lowIndex < pivotIndex)
-                pivotArray(arr, lowIndex, pivotIndex);
         }
 
         public static void Start(string[] args)
---
+++ Challenges/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Challenges
+{
+    public class Utils
+    {
+        public static void Quicksort<T>(T[] arr, int pivotIndex, int startIndex=0)
+        {
+            var lowIndex = startIndex;
+            for (int j = startIndex; j <= pivotIndex; j++)
+            {
+                if ((dynamic)arr[j] <= (dynamic)arr[pivotIndex])
+                {
+                    var currentValue = arr[lowIndex];
+                    arr[lowIndex] = arr[j];
+                    arr[j] = currentValue;
+                    lowIndex++;
+                }
+            }
+
+            if (lowIndex > 2)
+                Quicksort(arr,  lowIndex - 2, 0);
+            if (lowIndex < pivotIndex)
+                Quicksort(arr, pivotIndex, lowIndex);
+        }
+    }
+}
